bc8094f4382eace6e3aa343de69fcc23ffcf51af
Luis Acosta <luis.acosta@gnp.com.mx>
2021-05-21 17:49:39 -0500

    Se agrega funcion de copiar enlace

--- src/app/componnets/boton-option/boton-option.component.ts
+++ src/app/componnets/boton-option/boton-option.component.ts
@@ -12,6 +12,7 @@ export class BotonOptionComponent implements OnInit {
   @Input() type: string;
   @Input() data: any;
   @Input() grupo: boolean;
+  clickCopy: boolean = false;
 
   constructor(public dialog: MatDialog) { }
 
@@ -23,6 +24,22 @@ export class BotonOptionComponent implements OnInit {
       width: this.grupo ? '400px' : '400px',
       data: {type: this.type, info: this.data, grupo: this.grupo}
     });
+  }
+
+  copiarEnlace(): void {
+    this.clickCopy = true;
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.data.url;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    setTimeout(() => this.clickCopy = false, 1500);
   }
 
 }
